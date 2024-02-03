a7007ba67da2c18f06538013773a267a1ee940b5
Parasjmail <paras.jmail1234@gmail.com>
2024-02-03 10:59:57 +0530

    new commit

---
+++ 3letvarconst/letvarconst.js
@@ -0,0 +1,13 @@
+var a = 45;
+let b = "Paras";
+
+const author = "Paras";
+//let author = 5; //Not working
+let c = null;
+let d = undefined;
+
+{
+    let b = 'this';
+    console.log(b);
+}
+console.log(b);
