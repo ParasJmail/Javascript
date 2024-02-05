cfbd3d2cad4b740e920da4d7628759fd84497148
Parasjmail <paras.jmail1234@gmail.com>
2024-02-05 13:34:32 +0530

    new comit

---
+++ 4primitivedatatypes/primitive.js
@@ -0,0 +1,10 @@
+//nn bb ss u
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567");
+let e = "Paras";
+let f = Symbol("I am Paras.");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
