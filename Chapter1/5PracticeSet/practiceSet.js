1be6935a1e7257d206608ff1f2e0e5923e52c5dc
Parasjmail <paras.jmail1234@gmail.com>
2024-02-07 14:22:47 +0530

    new commit

---
+++ Chapter1/5PracticeSet/practiceSet.js
@@ -0,0 +1,29 @@
+//Chapter 1 Q1
+
+let a = "Paras";
+let b = 2;
+console.log(a+b);
+
+//Chapter 1 Q2
+console.log(typeof(a+b));
+
+//Chapter 1 Q3
+const a1 = {
+    name:"Paras",
+    section:1,
+    isPrincipal:false
+}
+
+//a1 = 23;
+
+//Chapter 1 Q4
+a1['friend'] = "Shubham";
+console.log(a1);
+
+//Chapter 1 Q5
+const dict = {
+    appreciate:"recognize the full worth of.",
+    good:"having the required qualities; of a high standard."
+}
+
+console.log(dict['good'])
