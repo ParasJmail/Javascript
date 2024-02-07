6c0b009c20d06e354c6881a80fda6c541c8ac593
Parasjmail <paras.jmail1234@gmail.com>
2024-02-07 14:10:21 +0530

    new commit

--- 4primitivedatatypes/primitive.js
+++ 4primitivedatatypes/primitive.js
@@ -1,3 +1,4 @@
+//Priitive Datatypes
 //nn bb ss u
 let a = null;
 let b = 345;
@@ -8,3 +9,16 @@ let f = Symbol("I am Paras.");
 let g = undefined;
 
 console.log(a,b,c,d,e,f,g);
+
+console.log(typeof(d));
+
+//Non Primitive Datatype = Object
+
+const item = {
+    "Paras":true,
+    "Harivansh":false,
+    "Bhart":23,
+    "Mugal":undefined
+}
+
+console.log(item["Paras"])
