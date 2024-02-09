1b07cca00610fc2e854866d937e854ce4bad401c
Parasjmail <paras.jmail1234@gmail.com>
2024-02-09 18:43:08 +0530

    new commit

---
+++ Chapter2/6operators/operators.js
@@ -0,0 +1,35 @@
+console.log("Operator in js");
+let a = 2;
+let b = 3;
+
+//Arithmethic Opeerator
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("++a",++a); //3
+console.log("a++",a++); //3
+console.log("a--",a--); //4
+console.log("--a",--a); //2
+
+//Assignment Operator
+a= 8;
+a+=5 //a = a+5;
+console.log(a);
+
+//Comparison Operator;
+let comp1 = 6;
+let comp2 = 7;
+
+console.log("comp1 == comp2 is ",comp1 == comp2);
+console.log("comp1 != comp2 is ",comp1 != comp2);
+console.log("comp1 === comp2 is ",comp1 === comp2);
+console.log("comp1 !== comp2 is ",comp1 !== comp2);
+
+//Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!false);
