fd38ee1d7ab592afe38637459034815d36403881
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-27 11:36:30 +0530

    new commit

---
+++ Chapter2/9forloops/for.js
@@ -0,0 +1,24 @@
+// for(let i = 0; i< 10;i++){
+//     console.log(i);
+// }
+
+//for in
+let obj = {
+    paras:12,
+    pavan:14,
+    asads:31,
+    asdsad:15
+}
+
+// for(let a in obj){
+//     console.log(a);
+// }
+
+// for(let a in obj){
+//     console.log(obj[a]);
+// }
+
+//for of
+for(let b of "paras"){
+    console.log(obj[a]);
+}
