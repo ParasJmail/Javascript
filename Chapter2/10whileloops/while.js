76a2621a8fa483a717bf4fe85fb3f5505844aec1
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-27 11:49:48 +0530

    new commit

---
+++ Chapter2/10whileloops/while.js
@@ -0,0 +1,24 @@
+// const prompt = require("prompt-sync")();
+
+// let n = prompt("enter value of n");
+// n = Number.parseInt(n);
+
+// let i = 0;
+// while(i < n){
+//     console.log(i);
+//     i++
+// }
+
+//do while
+
+const prompt = require("prompt-sync")();
+
+let n = prompt("enter value of n");
+n = Number.parseInt(n);
+
+let i = 0;
+
+do{
+    console.log(i);
+    i++
+}while(i<n);
