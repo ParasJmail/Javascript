5b75d53fc88ab5aa2dbc0dcc0bac4b45e40f6c2b
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-05-27 11:56:12 +0530

    new commit

---
+++ Chapter2/11functions/function.js
@@ -0,0 +1,16 @@
+function average(x,y){
+    return (x+y)/2;
+}
+
+
+let a =1;
+let b =2;
+let c =3;
+
+console.log("average of a & b "+ average(a,b));
+console.log("average of b & c "+ average(b,c));
+console.log("average of a & c "+ average(a,c));
+
+// console.log("average of a & b"+ (a+b)/2);
+// console.log("average of b & c"+ (b+c)/2);
+// console.log("average of a & c"+ (a+c)/2);
