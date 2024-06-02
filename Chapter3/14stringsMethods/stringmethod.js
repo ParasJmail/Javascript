fb362dccdccc42929fdfcf8cf2fc2a0b9f222e91
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-06-02 13:57:00 +0530

    new commit

---
+++ Chapter3/14stringsMethods/stringmethod.js
@@ -0,0 +1,18 @@
+let name = "paras"
+let a = name
+a= name.length
+a= name.toUpperCase()
+a= name.toLowerCase()
+a = name[0]
+a = name.slice(2,4)
+a = name.replace("par","har")
+
+// console.log(a);
+
+// console.log(name.concat(" is a friend ",a))
+
+let name1 = "            paavan           "
+
+let b = name1.trim();
+
+console.log(b);
