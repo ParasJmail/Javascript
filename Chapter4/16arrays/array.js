c030ddf9dfe20b9d7508d80121fb64ba84766dba
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-06-02 14:06:30 +0530

    new commit

---
+++ Chapter4/16arrays/array.js
@@ -0,0 +1,11 @@
+let marks = [82,91,83,99,false,null,"not present"];
+
+console.log(marks);
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+console.log(marks[5]);
+console.log(marks[6]);
+console.log(marks.length);
